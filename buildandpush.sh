6ba2e665580c2101663847e8354381b7d7c70808
GALVES <GALVES@GALVES-7410.br.oracle.com>
2021-07-24 18:15:20 -0300

    finalbash

--- buildandpush.sh
+++ buildandpush.sh
@@ -2,6 +2,7 @@
 
 docker build . -t devopsapp:latest
 
-docker tag $(docker images -q devopsapp:latest) $(oci iam region-subscription list | grep -oP '(?<="region-name": ")[^"]*').ocir.io/$(oci os ns get | grep -oP '(?<="data": ")[^"]*')/devopsworkshop/devopsapp:latest
+docker tag $(docker images -q devopsapp:latest) $(oci iam region-subscription list | grep -oP '(?<="region-key": ")[^"]*' | tr [:upper:] [:lower:]).ocir.io/$(oci os ns get | grep -oP '(?<="data": ")[^"]*')/devopsworkshop/devopsapp:latest
 
-docker push $(oci iam region-subscription list | grep -oP '(?<="region-name": ")[^"]*').ocir.io/$(oci os ns get | grep -oP '(?<="data": ")[^"]*')/devopsworkshop/devopsapp:latest
+docker push $(oci iam region-subscription list | grep -oP '(?<="region-key": ")[^"]*' | tr [:upper:] [:lower:]).ocir.io/$(oci os ns get | grep -oP '(?<="data": ")[^"]*')/devopsworkshop/devopsapp:latest
+
