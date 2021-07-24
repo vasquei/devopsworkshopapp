ae979da232413bb0d132d31b1bdd796f2bec2d44
GALVES <GALVES@GALVES-7410.br.oracle.com>
2021-07-24 16:45:52 -0300

    firstcommit

---
+++ index.js
@@ -0,0 +1,18 @@
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    console.log("Call received")
+    res.send({ "status": true })
+})
+
+app.get('/fail', (req, res) => {
+    console.log("Fail call received")
+    res.send({ "status": false })
+})
+
+app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`)
+})
