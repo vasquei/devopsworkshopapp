c90a0957966031a122d0e07f7072b5264b5a39ba
GALVES <GALVES@GALVES-7410.br.oracle.com>
2021-07-25 20:36:51 -0300

    final

--- buildandpush.sh
+++ buildandpush.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-echo "Informe o seu usuário:(Considere todo o valor apresentado na console ex. oracleidentitycloudservice/exemplo@exemplo.com)"
+echo "Informe o seu user ocid:(ex. ocid1.user.oc1.._______dincoha)"
 
-read cloudusername
+read clouduserocid
 
 echo "Realizando login no Oracle Container Registry..."
 
-docker login $(oci iam region-subscription list | grep -oP '(?<="region-key": ")[^"]*' | tr [:upper:] [:lower:]).ocir.io -u $(oci os ns get | grep -oP '(?<="data": ")[^"]*')/$cloudusername
+docker login $(oci iam region-subscription list | grep -oP '(?<="region-key": ")[^"]*' | tr [:upper:] [:lower:]).ocir.io -u $(oci os ns get | grep -oP '(?<="data": ")[^"]*')/$(oci iam user get --user-id ${clouduserocid} | grep -oP '(?<="name": ")[^"]*')
 
 echo "Construindo imagem de container..."
 
