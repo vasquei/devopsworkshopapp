5fb49610876cf6ae863c3dd3c1135dfe59b9b09b
GALVES <GALVES@GALVES-7410.br.oracle.com>
2021-07-26 15:58:59 -0300

    docker secret

--- createsecret.sh
+++ createsecret.sh
@@ -4,7 +4,7 @@ echo "Informe o seu user ocid:(ex. ocid1.user.oc1.._______dincoha)"
 read clouduserocid
 echo "Informe o seu Auth Token:"
 read authtoken
-kubectl create secret docker-registry ocir --docker-server=$(oci iam region-subscription list | grep -oP '(?<="region-key": ")[^"]*' | tr [:upper:] [:lower:]).ocir.io /
---docker-username= $(oci os ns get | grep -oP '(?<="data": ")[^"]*')/$(oci iam user get --user-id ${clouduserocid} | grep -oP '(?<="name": ")[^"]*') /
---docker-password='${authtoken}' /
+kubectl create secret docker-registry ocir --docker-server=$(oci iam region-subscription list | grep -oP '(?<="region-key": ")[^"]*' | tr [:upper:] [:lower:]).ocir.io \
+--docker-username= $(oci os ns get | grep -oP '(?<="data": ")[^"]*')/$(oci iam user get --user-id ${clouduserocid} | grep -oP '(?<="name": ")[^"]*') \
+--docker-password='${authtoken}' \
 --docker-email='example@example.com'
