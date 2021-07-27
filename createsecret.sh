1ce7299806e3bfa56fa6f04756fc8850d1745783
GALVES <GALVES@GALVES-7410.br.oracle.com>
2021-07-26 21:31:44 -0300

    manifestos

--- createsecret.sh
+++ createsecret.sh
@@ -3,6 +3,6 @@
 kubectl create namespace dev
 kubectl create namespace prod
 
-kubectl create secret -n dev generic ocir --from-file=.dockerconfigjson=./.docker/config.json --type=kubernetes.io/dockerconfigjson
+kubectl create secret -n dev generic ocir --from-file=.dockerconfigjson=../.docker/config.json --type=kubernetes.io/dockerconfigjson
 
-kubectl create secret -n prod generic ocir --from-file=.dockerconfigjson=./.docker/config.json --type=kubernetes.io/dockerconfigjson
+kubectl create secret -n prod generic ocir --from-file=.dockerconfigjson=../.docker/config.json --type=kubernetes.io/dockerconfigjson
